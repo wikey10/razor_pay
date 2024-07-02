bbb17922d449684184b6f99f2bded29780a947f6
wikey10 <vigneshmessi107@gmail.com>
2024-07-02 17:27:43 +0530

    Commit msg

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Razor Pay Demos',
+      title: 'Razor Pay Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
